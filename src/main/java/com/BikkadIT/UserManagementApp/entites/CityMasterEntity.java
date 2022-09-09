07542c50f5028a430d49f77520e4af9422710393
ms456143 <ms456143@gmail.com>
2022-09-08 19:50:01 -0700

    UMA:Create below packages in the project

---
+++ src/main/java/com/BikkadIT/UserManagementApp/entites/CityMasterEntity.java
@@ -0,0 +1,5 @@
+package com.BikkadIT.UserManagementApp.entites;
+
+public class CityMasterEntity {
+
+}
---
+++ src/test/java/com/BikkadIT/UserManagementApp/UserManagementApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.UserManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
